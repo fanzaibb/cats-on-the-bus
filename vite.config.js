9331b49b963dc8e2f8f914075ee82a9bf503c893
fan <fan11204@gmail.com>
2021-11-30 01:21:25 +0800

    feat: set config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/cats-on-the-bus/',
+    base: '/cats-on-the-bus/',
     plugins: [vue()],
     resolve: {
         alias: {
